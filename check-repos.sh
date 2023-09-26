9bcb18350da603bf3c65281d362a3dfb9a9db0d8
Tomasz Laboj <133808759+TomaszLaboj@users.noreply.github.com>
2023-09-26 15:55:52 +0100

    Add files via upload
    add sh script to loop over the subdirectories

---
+++ check-repos.sh
@@ -0,0 +1,26 @@
+1#!/bin/bash
+
+# Path to the folder containing all git repos
+repo_folder="/home/2306-13-tl/Developer/academy/training"
+
+#list all subdirectories
+
+repo_paths=($(find "$repo_folder" -mindepth 1 -maxdepth 1 -type d))
+
+for repo_path in "${repo_paths[@]}"; do
+	echo "Checking repo : $repo_path"
+	cd "$repo_path" || continue
+
+	# list all local branches
+	local_branches=$(git branch | cut -c 3-)
+
+	for branch in $local_branches; do
+		#check if the branch exists on the remote
+		if ! git ls-remote --exit-code --heads origin "$branch" >/dev/null 2>&1; then
+		echo "Branch '$branch' not pushed to remote in $repo_path"
+
+		fi
+	done
+
+	cd - >/dev/null
+done
